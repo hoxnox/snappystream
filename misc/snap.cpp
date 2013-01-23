af36174a38af628f07c6a9e1e3faa43e5833e79d
Merder Kim <hoxnox@gmail.com>
2013-01-23 09:40:29 +0400

    Simplest cmd archiver added

---
+++ misc/snap.cpp
@@ -0,0 +1,24 @@
+/**@author Merder Kim <hoxnox@gmail.com>
+ * @date 20130123 09:40:29 
+ * @brief Simplest SnappyStream based cmd archiver*/
+
+#include <iostream>
+#include <sstream>
+#include <snappystream.hpp>
+
+using namespace snappy;
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " x|c" << std::endl;
+		return 1;
+	}
+	if(argv[1][0] == 'x')
+		std::cout << iSnappyStream(std::cin).rdbuf();
+	else if(argv[1][0] == 'c')
+		oSnappyStream(std::cout) << std::cin.rdbuf();
+	return 0;
+}
+
