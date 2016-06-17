7b5f8aef9e04fca3ae4f9af30338be06030e4f94
hoxnox <hoxnox@gmail.com>
2016-06-17 13:51:03 +0300

    ENH: "conanized"

---
+++ test_package/conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, CMake
+import os
+
+channel = os.getenv("CONAN_CHANNEL", "testing")
+username = os.getenv("CONAN_USERNAME", "hoxnox")
+
+class SnappyStreamTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "snappystream/0.2.2@%s/%s" % (username, channel)
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def imports(self):
+        self.copy("*.dll", "bin", "bin")
+        self.copy("*.dylib", "bin", "lib")
+
+    def test(self):
+        os.chdir("bin")
+        self.run(".%stest" % os.sep)
