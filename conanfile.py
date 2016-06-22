7ac4073d302551e1785cefaf408e914294dc0052
hoxnox <hoxnox@gmail.com>
2016-06-22 09:53:45 +0300

    ENH: add url and correct export to conanfile

--- conanfile.py
+++ conanfile.py
@@ -8,14 +8,21 @@ class SnappyStreamConan(ConanFile):
     options = {"boost_iostreams": [True, False]}
     default_options = "boost_iostreams=False", "snappy:shared=False"
     generators = "cmake"
-    exports = "*"
+    exports = ("include/*.hpp",
+               "src/*.hpp", "src/*.cpp",
+               "misc/snap.cpp",
+               "snappystream.cfg",
+               "cmake/ext/nx_utils.cmake",
+               "CMakeLists.txt",
+               "README.markdown")
+    url = "https://github.com/hoxnox/snappystream.git"
 
     def build(self):
         cmake = CMake(self.settings)
         boost_iostreams_definition = ""
         if self.options.boost_iostreams:
-            boost_iostreams_definition = "-DWITH_BOOST_IOSTREAMS=1" 
-        self.run('cmake %s -DWITH_CONAN=1 -DCMAKE_INSTALL_PREFIX=./distr %s %s' % 
+            boost_iostreams_definition = "-DWITH_BOOST_IOSTREAMS=1"
+        self.run('cmake %s -DWITH_CONAN=1 -DCMAKE_INSTALL_PREFIX=./distr %s %s' %
                 (self.conanfile_directory, cmake.command_line, boost_iostreams_definition))
         self.run("cmake --build . %s" % cmake.build_config)
         self.run("make install")
